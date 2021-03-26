5a0559996697c2c1024a684448a0510709fc143b
LabbeAramis <labbe.aramis@gmail.com>
2021-03-26 13:11:57 +0500

    added register listeners from module.json

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -4,6 +4,7 @@ namespace LabbeAramis\Modules\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use LabbeAramis\Modules\Facades\Event;
+use LabbeAramis\Modules\Contracts\RepositoryInterface;
 
 /**
  * Class EventServiceProvider
@@ -34,5 +35,51 @@ class EventServiceProvider extends ServiceProvider
                 Event::subscribe( $subscriber );
             }
         } );
+    }
+
+    /**
+     * Boot any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        $this->registerListeners();
+    }
+
+    /**
+     * @return array
+     */
+    public function registerListeners(): array
+    {
+
+        $laravelFileRepository = app( RepositoryInterface::class );
+
+        $listeners = [];
+
+        foreach ($laravelFileRepository->scan() as $module) {
+            $moduleListeners = $module->get( 'listeners' );
+            if ( $moduleListeners !== null
+                && is_array( $moduleListeners ) === true
+                && count( $moduleListeners ) > 0 ) {
+                foreach ($moduleListeners as $event => $listener) {
+                    if ( is_array( $listener ) === true && count( $listener ) > 0 ) {
+                        foreach ($listener as $className) {
+                            if ( class_exists( $className, true ) === true ) {
+                                $listeners[$event][] = $className;
+
+                                Event::listen(
+                                    $event,
+                                    [$className, 'handle']
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return $listeners;
     }
 }
