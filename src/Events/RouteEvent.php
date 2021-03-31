573978490b4735c51d68163b3b7eadafaf8fdbe6
o.antipin <o.antipin@true-studio.ru>
2021-03-31 16:59:12 +0500

    fixed route event constructor

--- src/Events/RouteEvent.php
+++ src/Events/RouteEvent.php
@@ -9,6 +9,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Routing\Controller;
 use LabbeAramis\Modules\Event;
 use LabbeAramis\Modules\Traits\Dispatchable;
+use ReflectionParameter;
 
 /**
  * Class RouteEvent
@@ -37,15 +38,28 @@ class RouteEvent extends Event
     /**
      * RouteEvent constructor.
      *
-     * @param Controller   $controller
-     * @param string       $method
-     * @param Request|null $request
+     * @param Controller|string $controller
+     * @param string            $method
+     * @param Request|null      $request
+     *
+     * @throws \ReflectionException
+     * @throws \Exception
      */
-    public function __construct(Controller $controller, string $method, Request $request = null)
+    public function __construct($controller, string $method, Request $request = null)
     {
-        $this->controller = $controller;
         $this->method = $method;
         $this->request = $request;
+
+        if ( $controller instanceof Controller ) {
+
+            $this->controller = $controller;
+        } elseif ( gettype($controller) === 'string' ) {
+
+            $this->controller = $this->createObject($controller);
+        } else {
+
+            throw new \Exception('Can\'t create object');
+        }
     }
 
     /**
@@ -70,5 +84,57 @@ class RouteEvent extends Event
     public function getRequest()
     {
         return $this->request;
+    }
+
+    /**
+     * @param string $className
+     *
+     * @return object
+     * @throws \ReflectionException
+     */
+    protected function createObject(string $className)
+    {
+
+        if ( class_exists($className, true) === true ) {
+
+            $reflection = new \ReflectionClass($className);
+
+            if ( $reflection->isInstantiable() === true ) {
+
+                $constructor = $reflection->getConstructor();
+                $constructorParams = $constructor->getParameters();
+
+                $getDependencies = function(array $params = []) use ($className) {
+
+                    $dependencies = [];
+
+                    /** @var ReflectionParameter $param */
+                    foreach ($params as $param) {
+
+                        $class = $param->getDeclaringClass();
+                        if ( $class !== null ) {
+                            $paramClassName = $class->getName();
+                            $dependencies[] = $this->createObject($paramClassName);
+                        } elseif ( $param->hasType() === true ) {
+                            if ( $param->isDefaultValueAvailable() === true ) {
+                                $dependencies[] = $param->getDefaultValue();
+                            } elseif ( $param->allowsNull() === true ) {
+                                $dependencies[] = null;
+                            } else {
+                                throw new \Exception('Can\'t create object "' . $className . '"');
+                            }
+                        } elseif ( $param->allowsNull() === true ) {
+                            $dependencies[] = null;
+                        } else {
+                            throw new \Exception('Can\'t create object "' . $className . '"');
+                        }
+                    }
+
+                    return $dependencies;
+                };
+
+                return $reflection->newInstance(...$getDependencies($constructorParams));
+            }
+        }
     }
 }
